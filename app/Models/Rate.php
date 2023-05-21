6ba6a8623882b325c384eefc0a46ad83f0bcea85
Dana Horani <danaahorani@gmail.com>
2023-05-21 20:11:14 +0300

    add Rate model & migration

---
+++ app/Models/Rate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Rate extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'rate',
+    ];
+
+    /**
+     * Get the user that owns the Rate
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    
+    /**
+     * Get the product that owns the Rate
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+}
