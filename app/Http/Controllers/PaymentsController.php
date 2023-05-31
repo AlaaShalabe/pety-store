caa95ac0c419bb8020ffc718f42479a0524d7f17
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-05-31 12:51:27 +0300

    fix Auth

---
+++ app/Http/Controllers/PaymentsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class PaymentsController extends Controller
+{
+    public function create(Order $order)
+    {
+        return [
+            'order' => $order
+        ];
+    }
+}
