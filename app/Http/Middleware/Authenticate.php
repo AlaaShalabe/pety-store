4113c6b5d708948772a627724d9f79399f55ab01
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-06-02 23:26:25 +0300

    create authentcation middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (!auth()->check()) {
+            return abort(403);
         }
+        return $next($request);
     }
 }
